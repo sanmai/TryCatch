34658454e6eee385233d20754a9d31d6ddb133c9
Alexey Kopytko <alexey@kopytko.com>
2020-01-30 05:42:09 +0900

    CS
    Signed-off-by: Alexey Kopytko <alexey@kopytko.com>

--- src/TryCatch.php
+++ src/TryCatch.php
@@ -1,8 +1,29 @@
 <?php
-/*
- * Copyright (c) 2016 Alexey Kopytko
- * Released under the MIT license.
+/**
+ * This code is licensed under the MIT License. (MIT)
+ *
+ * Copyright (c) 2016 Alexey Kopytko <alexey@kopytko.ru>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
+declare(strict_types=1);
 
 namespace TryCatch;
 
@@ -16,6 +37,7 @@ class TryCatch
 
     /**
      * @param callable $func Callback to execute when invoked
+     *
      * @return TryCatch
      */
     public static function wrap(callable $func)
@@ -25,7 +47,6 @@ class TryCatch
 
     /**
      * @see TryCatch::wrap
-     * @param callable $func
      */
     public function __construct(callable $func)
     {
@@ -34,30 +55,28 @@ class TryCatch
 
     /**
      * @param callable $func Callable to execute if any exception is found
+     *
      * @return TryCatch
      */
     public function whenFailed(callable $func)
     {
         $this->whenFailed = $func;
+
         return $this;
     }
 
     /**
-     * Executes a callable and capture any exceptions while returning the result of execution
+     * Executes a callable and capture any exceptions while returning the result of execution.
      */
     public function __invoke(...$args)
     {
-
         try {
-            return call_user_func_array($this->callback, $args);
+            return \call_user_func_array($this->callback, $args);
         } catch (\Exception $e) {
             if ($this->whenFailed) {
-                return call_user_func($this->whenFailed, $e, ...$args);
-            } else {
-                throw $e;
+                return \call_user_func($this->whenFailed, $e, ...$args);
             }
+            throw $e;
         }
     }
 }
-
-
--- tests/TryCatchTest.php
+++ tests/TryCatchTest.php
@@ -1,13 +1,37 @@
 <?php
-/*
- * Copyright (c) 2016 Alexey Kopytko
- * Released under the MIT license.
+/**
+ * This code is licensed under the MIT License. (MIT)
+ *
+ * Copyright (c) 2016 Alexey Kopytko <alexey@kopytko.ru>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
+declare(strict_types=1);
 
 namespace Tests\TryCatch;
 
 use TryCatch\TryCatch;
 
+/**
+ * @covers \TryCatch\TryCatch
+ */
 class TryCatchTest extends \PHPUnit\Framework\TestCase
 {
     public function testSimple()
@@ -22,7 +46,7 @@ class TryCatchTest extends \PHPUnit\Framework\TestCase
     public function testWithAnArg()
     {
         $wrapped = TryCatch::wrap(function ($arg) {
-            return 2*$arg;
+            return 2 * $arg;
         });
 
         $this->assertEquals(4, $wrapped(2));
@@ -31,7 +55,7 @@ class TryCatchTest extends \PHPUnit\Framework\TestCase
     public function testUncaught()
     {
         $wrapped = TryCatch::wrap(function () {
-            throw new \Exception("Unexpected");
+            throw new \Exception('Unexpected');
         });
 
         $this->expectException(\Exception::class);
@@ -43,7 +67,7 @@ class TryCatchTest extends \PHPUnit\Framework\TestCase
     public function testCaught()
     {
         $wrapped = TryCatch::wrap(function () {
-            throw new \Exception("Unexpected");
+            throw new \Exception('Unexpected');
         })->whenFailed(function (\Exception $e) {
             $this->assertEquals('Unexpected', $e->getMessage());
         });
@@ -56,14 +80,16 @@ class TryCatchTest extends \PHPUnit\Framework\TestCase
         $numerator = 100;
 
         $fraction = function ($arg) use ($numerator) {
-            if ($arg == 0) {
-                throw new \Exception("Division by zero");
+            if ($arg === 0) {
+                throw new \Exception('Division by zero');
             }
-            return $numerator/$arg;
+
+            return $numerator / $arg;
         };
 
         $wrapped = TryCatch::wrap($fraction)->whenFailed(function (\Exception $e, $arg) {
             $this->assertEquals(0, $arg);
+
             return null;
         });
 
@@ -74,17 +100,18 @@ class TryCatchTest extends \PHPUnit\Framework\TestCase
     public function testFromReadme()
     {
         $wrapped = TryCatch::wrap(function ($divisor) {
-            if ($divisor == 0) {
-                throw new \Exception("Division by zero");
+            if ($divisor === 0) {
+                throw new \Exception('Division by zero');
             }
-            return M_PI/$divisor;
+
+            return M_PI / $divisor;
         })->whenFailed(function (\Exception $e, $divisor) {
             $this->assertEquals(0, $divisor);
+
             return null;
         });
 
-        $this->assertEquals(M_PI/2, $wrapped(2));
+        $this->assertEquals(M_PI / 2, $wrapped(2));
         $this->assertNull($wrapped(0));
     }
 }
-
